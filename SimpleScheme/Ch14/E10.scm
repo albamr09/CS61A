f11226a8be280eb3f4722d7f05e0c767e2b810e0
albamr09 <bertaalba1@gmail.com>
2021-11-16 18:04:14 +0100

    Some Ch14 SS exercises

--- SimpleScheme/Ch14/E10.scm
+++ SimpleScheme/Ch14/E10.scm
@@ -2,10 +2,17 @@
 ; an argument and returns the number of words in the sentence that are immediately
 ; followed by the same word:
 
-(define (count-adjacent-duplicates-helpter sent count)
-)
-
 (define (count-adjacent-duplicates sent)
+    (cond
+        ((empty? sent) 0)
+        ((= (count sent) 1) 0)
+        (else 
+            (if (equal? (first sent) (first (bf sent)))
+                (+ 1 (count-adjacent-duplicates (bf sent)))
+                (+ 0 (count-adjacent-duplicates (bf sent)))
+            )
+        )
+    )
 )
 
 (count-adjacent-duplicates '(y a b b a d a b b a d o o))
---
+++ SimpleScheme/Ch14/E14.scm
@@ -0,0 +1,23 @@
+; Write a predicate that takes two sentences as arguments. It should
+; return if two conditions are met: The two sentences must have the same number of
+; words, and each word of the first sentence must have the same number of letters as the
+; word in the corresponding position in the second sentence.
+
+(define (same-shape-helper sent1 sent2)
+    (cond
+      ((empty? sent1) #t)
+      ((= (count (first sent1)) (count (first sent2))) (same-shape-helper (bf sent1) (bf sent2)))
+      (else #f)
+    )
+)
+
+(define (same-shape? sent1 sent2)
+    (if (equal? (count sent1) (count sent2))
+        (same-shape-helper sent1 sent2)
+        #f
+    )
+)
+(same-shape? '(the fool on the hill) '(you like me too much))
+; #T
+(same-shape? '(the fool on the hill) '(and your bird can sing))
+; #F
