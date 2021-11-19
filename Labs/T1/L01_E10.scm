9027440de073df4ce97e621086bd21402f67e683
albamr09 <bertaalba1@gmail.com>
2021-11-19 17:36:24 +0100

    Finish Lab1 from Unit 1

---
+++ Labs/T1/L01_E10.scm
@@ -0,0 +1,14 @@
+; Write a procedure ends-e that takes a sentence as its argument and returns a
+; sentence containing only those words of the argument whose last letter is E.
+
+;; Exercise 10.
+(define (ends-e sent)
+  (cond
+    ((empty? sent) '())
+    ((equal? (last (first sent)) 'e) (se (first sent) (ends-e (bf sent))))
+    (else (ends-e (bf sent)))
+  )
+)
+(ends-e '(please put the salami above the blue elephant))
+;; Outputs: (please the above the blue)
+ 
