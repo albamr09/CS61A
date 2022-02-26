b9670c4e8ec8ef8a45223a49eb9164f2d613c520
Alba Martínez Rodríguez <bertaalba1@gmail.com>
2022-02-26 19:47:51 +0100

    [Book] Start T8

---
+++ BProblems/T3/P01_01.rkt
@@ -0,0 +1,168 @@
+#lang racket
+(require berkeley)
+
+(define balance 100)
+
+(define (withdraw amount)
+  (if (>= balance amount)
+    (begin 
+      ; Update the value of balance
+      (set! balance (- balance amount))
+      ; Return the value
+      balance
+    )
+    "Insufficient funds"
+  )
+)
+
+; (withdraw 25)
+; ; 75
+; (withdraw 25)
+; ; 50
+; (withdraw 60)
+; ; "Insufficient funds"
+; (withdraw 15)
+; ; 35
+
+;;;;;;;;;;;;;;;;;
+; balance as a local variable
+;;;;;;;;;;;;;;;;;
+
+(define new-withdraw
+  ; Bind balance
+  (let ((balance 100))
+    (lambda (amount)
+      (if (>= balance amount)
+        (begin 
+          (set! balance (- balance amount))
+          balance
+        )
+        "Insufficient funds"
+      )
+    )
+  )
+)
+
+; (new-withdraw 25)
+; ; 75
+; (new-withdraw 25)
+; ; 50
+; (new-withdraw 60)
+; ; "Insufficient funds"
+; (new-withdraw 15)
+; ; 35
+
+(define (make-withdraw balance)
+  (lambda (amount)
+    (if (>= balance amount)
+      (begin 
+        (set! balance (- balance amount))
+        balance
+      )
+      "Insufficient funds"
+    )
+  )
+)
+
+; (define W1 (make-withdraw 100))
+; (define W2 (make-withdraw 100))
+
+; (W1 50)
+; ; 50
+; (W2 70)
+; ; 30
+; (W2 40)
+; ; "Insufficient funds"
+; (W1 40)
+; ; 10
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ACCOUNT WITH MESSAGE PASSING
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (make-account balance)
+  ; Augment the amount in the account
+  (define (withdraw amount)
+    (if (>= balance amount)
+      (begin 
+        (set! balance (- balance amount))
+        balance
+      )
+      "Insufficient funds"
+    )
+  )
+
+  ; Reduce the amount in the account
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance
+  )
+
+  ; Dispatch one of the methods given
+  ; the operation name: "m"
+  (define (dispatch m)
+    (cond 
+      ; Return one procedure or the other
+      ; based on the operation name
+      ((eq? m 'withdraw) withdraw)
+      ((eq? m 'deposit) deposit)
+      (else 
+        (error "Unknown request: MAKE-ACCOUNT" m)
+      )
+    )
+  )
+  ; Return the procedure dispatch
+  ; that handles the message passing
+  dispatch ; Value that represents the bank-account object
+)
+
+(define acc (make-account 100))
+
+; ((acc 'withdraw) 50)
+; ; 50
+; ((acc 'withdraw) 60)
+; ; "Insufficient funds"
+; ((acc 'deposit) 40)
+; ; 90
+; ((acc 'withdraw) 60)
+; ; 30
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ BProblems/T3/P01_02.rkt
@@ -0,0 +1,123 @@
+#lang racket
+(require berkeley)
+
+; Auxilary function to generate random numbers
+(define (rand-update x)
+  (let 
+    ((a 27) (b 26) (m 127))
+    (modulo (+ (* a x) b) m)
+  )
+)
+
+; Define random-init to be a fixed number
+(define random-init 21)
+
+; Generate random numbers
+
+(define rand 
+  ; Initialize x
+  (let ((x random-init))
+    (lambda ()
+      ; Update the value of x
+      (set! x (rand-update x))
+      ; Return x
+      x
+    )
+  )
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MONTE CARLO METHOD
+
+; ----
+;; Explanation
+; ----
+
+; The Monte Carlo method consists of choosing sample experiments
+; at random from a large set and then making deductions on the basis of
+; the probabilities estimated from tabulating the results of those experiments. 
+
+; ---
+;; Example of application
+; ---
+
+; For example, we can approximate π using the fact that 6/π² is
+; the probability that two integers chosen at random will have no factors in 
+; common; that is, that their greatest common divisor will be 1.
+
+; To obtain the approximation to π, we perform a large number of experiments. 
+; In each experiment we choose two integers at random and
+; perform a test to see if their gcd is 1. e fraction of times that the test
+; is passed gives us our estimate of 6/π², and from this we obtain our approximation to π.
+
+
+; - trials: number of times to run the experiment
+; - experiment: procedure with no arguments that 
+;               executes the experiment and returns true
+;               if the experiment was successfull or false
+;               otherwise
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond 
+      ; If we have already run the experiment all of the 
+      ; specified times, finish
+      ((= trials-remaining 0)
+        ; Return the ratio of success vs times run
+        (/ trials-passed trials)
+      )
+      ; If the experiment was successfull
+      ((experiment)
+        ; Continue the approximation
+        (iter 
+          ; Update the times left to run
+          (- trials-remaining 1)
+          ; Update the successfull trials
+          (+ trials-passed 1)
+        )
+      )
+      ; If the experiment was no successfull
+      (else
+        ; Continue
+        (iter 
+          ; Update the times left to run
+          (- trials-remaining 1)
+          ; Do not update the successfull trials
+          trials-passed
+        )
+      )
+    )
+  )
+  ; Start the iterative approximation
+  (iter trials 0)
+)
+
+
+;;;;
+; TEST
+;;;;
+
+(define (estimate-pi trials)
+  ; 6/π² is the probability that two
+  ; random numbers will be relatively prime
+  ; Resolve the equation to obtain the pi estimate
+  (sqrt 
+    (/ 6 
+      ; Obtain the approximation for 6/π²
+      (monte-carlo 
+        ; Number of times to perform the experiment
+        trials 
+        ; Experiment (is the gcd of two random numbers a, b equal to 1?)
+        cesaro-test
+      )
+    )
+  )
+)
+
+; Are two random numbers a, b relatively prime?
+; as is: gcd(a, b) = 1
+(define (cesaro-test)
+  (= (gcd (rand) (rand)) 1)
+)
+
+; (estimate-pi 10000)
