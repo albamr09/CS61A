0c602bc9af1e38376889efb8695476c34bb99ff8
Alba Martínez Rodríguez <bertaalba1@gmail.com>
2022-03-12 12:32:25 +0100

    [Project] Continue P03

--- Projects/03/adv-world.scm
+++ Projects/03/adv-world.scm
@@ -1,3 +1,4 @@
+(load "./adv.scm")
 ;;;  Data for adventure game.  This file is adv-world.scm
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -23,7 +24,7 @@
 (can-go art-gallery 'west BH-Office)
 (can-go BH-Office 'east art-gallery)
 (can-go art-gallery 'east MJC-Office)
-(can-go MJC-office 'west art-gallery)
+(can-go MJC-Office 'west art-gallery)
 (can-go Soda 'down 61A-Lab)
 (can-go 61A-Lab 'up Soda)
 (can-go Soda 'south Pimentel)
@@ -48,35 +49,54 @@
 ; -- Ryan Stejskal
 
 (define Brian (instantiate person 'Brian BH-Office))
-(define hacker (instantiate person 'hacker 61A-lab))
-(define nasty (instantiate thief 'nasty sproul-plaza))
+(define hacker (instantiate person 'hacker 61A-Lab))
+(define nasty (instantiate thief 'nasty Sproul-Plaza))
 
 (define (sproul-hall-exit)
-   (error "You can check out any time you'd like, but you can never leave"))
+  (error "You can check out any time you'd like, but you can never leave")
+)
 
 (define (bh-office-exit)
   (print "What's your favorite programming language?")
   (let ((answer (read)))
     (if (eq? answer 'scheme)
-	(print "Good answer, but my favorite is Logo!")
-	(begin (newline) (bh-office-exit)))))
+	    (print "Good answer, but my favorite is Logo!")
+	    (begin 
+        (newline) 
+        (bh-office-exit)
+      )
+    )
+  )
+)
     
 
 
 (ask s-h 'add-entry-procedure
- (lambda () (print "Miles and miles of students are waiting in line...")))
+ (lambda () (print "Miles and miles of students are waiting in line..."))
+)
+
 (ask s-h 'add-exit-procedure sproul-hall-exit)
 (ask BH-Office 'add-exit-procedure bh-office-exit)
+
 (ask Noahs 'add-entry-procedure
- (lambda () (print "Would you like lox with it?")))
+ (lambda () (print "Would you like lox with it?"))
+)
+
 (ask Noahs 'add-exit-procedure
- (lambda () (print "How about a cinnamon raisin bagel for dessert?")))
+ (lambda () (print "How about a cinnamon raisin bagel for dessert?"))
+)
+
 (ask Telegraph-Ave 'add-entry-procedure
- (lambda () (print "There are tie-dyed shirts as far as you can see...")))
+ (lambda () (print "There are tie-dyed shirts as far as you can see..."))
+)
+
 (ask 61A-Lab 'add-entry-procedure
- (lambda () (print "The computers seem to be down")))
+ (lambda () (print "The computers seem to be down"))
+)
+
 (ask 61A-Lab 'add-exit-procedure
- (lambda () (print "The workstations come back to life just in time.")))
+ (lambda () (print "The workstations come back to life just in time."))
+)
 
 ;; Some things.
 
@@ -85,3 +105,112 @@
 
 (define coffee (instantiate thing 'coffee))
 (ask Intermezzo 'appear coffee)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; E01
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; 1. Instantiate a new Person object to represent yourself. 
+; 2. Put yourself in a new place called dormitory (or wherever you live) and 
+; 3. connect it to campus so that it is a reachable place. 
+; 4. Create a place called kirin, north of soda. (It's actually on Solano Avenue.) 
+; 5. Put a thing called potstickers there. 
+; 6. Then give the necessary commands to move your character to kirin, 
+; 7. take the potstickers, 
+; 8. then move yourself to where Brian is, 
+; 9. put down the potstickers, 
+; 10. and have Brian take them. 
+; 11. Then go back to the lab and get back to work. 
+
+; (There is no truth to the rumor that you'll get an A in the course for doing this in real life!) 
+; All this is just to ensure that you know how to speak the language of the adventure program.
+
+; List all messages that are sent during this episode. It's a good idea to see if you can work this out 
+; in your head, at least for some of the actions that take place, but you can also trace the ask procedure to get a 
+; complete list. You don't have to hand in this listing of messages. (You must turn in a transcript of the episode 
+; without the tracing.) The purpose of this exercise is to familiarize you with the ways in which the different 
+; objects send messages back and forth as they do their work.
+
+; [Tip: we have provided a move-loop procedure that you may find useful as an aid in debugging your work. You can use it to move a person repeatedly.]
+
+;;; 1. Instantiate a new Person object to represent yourself. 
+;;; 2. Put yourself in a new place called dormitory (or wherever you live) and 
+
+; Create the place where i live
+(define SimCity (instantiate place 'sim-city))
+
+; Create myself, indicate where i live
+(define Alba (instantiate person 'Alba SimCity))
+
+;;; 3. connect it to campus so that it is a reachable place. 
+
+(can-go SimCity 'down Soda)
+
+;;; 4. Create a place called kirin, north of soda. (It's actually on Solano Avenue.) 
+
+(define Kirin (instantiate place 'kirin))
+
+; Kirin is north of art-gallery and art-gallery is north of Soda, so by transitivity Kirin 
+; is north of Soda
+(can-go Kirin 'down art-gallery)
+(can-go art-gallery 'up Kirin)
+
+;; 5. Put a thing called potstickers there. 
+
+(define potstickers (instantiate thing 'potstickers))
+(ask Kirin 'appear potstickers)
+
+;;; 6. Then give the necessary commands to move your character to kirin, 
+
+; Interactive method:
+; (move-loop Alba)
+; - down: Move from SimCity to Soda
+; - up: Move from Soda to art-gallery
+; - up: Move from art-gallery to Kirin
+; - stop: Finish moving
+; Direct method:
+(ask Alba 'go 'down)
+(ask Alba 'go 'up)
+(ask Alba 'go 'up)
+
+; 7. take the potstickers
+
+(ask Alba 'take potstickers)
+
+; 8. then move yourself to where Brian is, 
+
+; Interactive method:
+; (move-loop Alba)
+
+; Brian is in BH-Office
+; - down: Move from Kirin to art-gallery
+; - west: Move from art-gallery to BH-Office
+; - stop: Finish moving
+
+; Direct method:
+(ask Alba 'go 'down)
+(ask Alba 'go 'west)
+
+; 9. put down the potstickers, 
+
+(ask Alba 'lose potstickers)
+
+; 10. and have Brian take them. 
+
+(ask Brian 'take potstickers)
+
+; 11. Then go back to the lab and get back to work. 
+
+; Interactive method
+; (move-loop Alba)
+; - east: Move from BH-Office to art-gallery
+; - down: Move from art-gallery to Soda
+; - down: Move from Soda to 61A-Lab
+; - stop: Finish moving
+
+; Direct method:
+(ask Alba 'go 'east)
+(ask Alba 'go 'down)
+(ask Alba 'go 'down)
+
--- Projects/03/adv.scm
+++ Projects/03/adv.scm
@@ -1,174 +1,316 @@
+(load "../../lib/obj.scm")
+
 ;; ADV.SCM
 ;; This file contains the definitions for the objects in the adventure
 ;; game and some utility procedures.
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PLACE
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define-class (place name)
+  ; Instance variables
   (instance-vars
-   (directions-and-neighbors '())
-   (things '())
-   (people '())
-   (entry-procs '())
-   (exit-procs '()))
+    (directions-and-neighbors '())
+    (things '())
+    (people '())
+    (entry-procs '())
+    (exit-procs '())
+  )
+
+  ; Methods
   (method (type) 'place)
   (method (neighbors) (map cdr directions-and-neighbors))
   (method (exits) (map car directions-and-neighbors))
+
   (method (look-in direction)
-    (let ((pair (assoc direction directions-and-neighbors)))
+    (let 
+      ((pair (assoc direction directions-and-neighbors)))
       (if (not pair)
-	  '()                     ;; nothing in that direction
-	  (cdr pair))))           ;; return the place object
+	      '()                     ;; nothing in that direction
+	      (cdr pair)              ;; return the place object
+      )
+    )
+  )           
+
   (method (appear new-thing)
     (if (memq new-thing things)
-	(error "Thing already in this place" (list name new-thing)))
+	    (error "Thing already in this place" (list name new-thing))
+    )
     (set! things (cons new-thing things))
-    'appeared)
+    'appeared
+  )
+
   (method (enter new-person)
     (if (memq new-person people)
-	(error "Person already in this place" (list name new-person)))
+	    (error "Person already in this place" (list name new-person))
+    )
     (set! people (cons new-person people))
-    (for-each (lambda (proc) (proc)) entry-procs)
-    'appeared)
+    (for-each 
+      (lambda (proc) (proc)) 
+      entry-procs
+    )
+    'appeared
+  )
+
   (method (gone thing)
     (if (not (memq thing things))
-	(error "Disappearing thing not here" (list name thing)))
+	    (error "Disappearing thing not here" (list name thing))
+    )
     (set! things (delete thing things)) 
-    'disappeared)
+    'disappeared
+  )
+  
   (method (exit person)
-    (for-each (lambda (proc) (proc)) exit-procs)
+    (for-each 
+      (lambda (proc) (proc)) 
+      exit-procs
+    )
     (if (not (memq person people))
-	(error "Disappearing person not here" (list name person)))
+	    (error "Disappearing person not here" (list name person))
+    )
     (set! people (delete person people)) 
-    'disappeared)
+    'disappeared
+  )
 
   (method (new-neighbor direction neighbor)
     (if (assoc direction directions-and-neighbors)
-	(error "Direction already assigned a neighbor" (list name direction)))
+	    (error "Direction already assigned a neighbor" (list name direction))
+    )
     (set! directions-and-neighbors
-	  (cons (cons direction neighbor) directions-and-neighbors))
-    'connected)
+	    (cons 
+        (cons direction neighbor) 
+        directions-and-neighbors
+      )
+    )
+    'connected
+  )
 
   (method (add-entry-procedure proc)
-    (set! entry-procs (cons proc entry-procs)))
+    (set! entry-procs (cons proc entry-procs))
+  )
   (method (add-exit-procedure proc)
-    (set! exit-procs (cons proc exit-procs)))
+    (set! exit-procs (cons proc exit-procs))
+  )
   (method (remove-entry-procedure proc)
-    (set! entry-procs (delete proc entry-procs)))
+    (set! entry-procs (delete proc entry-procs))
+  )
   (method (remove-exit-procedure proc)
-    (set! exit-procs (delete proc exit-procs)))
+    (set! exit-procs (delete proc exit-procs))
+  )
   (method (clear-all-procs)
     (set! exit-procs '())
     (set! entry-procs '())
-    'cleared) )
+    'cleared
+  ) 
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; PERSON
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-class (person name place)
+  ; Instance variables
   (instance-vars
-   (possessions '())
-   (saying ""))
+    (possessions '())
+    (saying "")
+  )
+
+  ; Instantiation variables
   (initialize
-   (ask place 'enter self))
+   (ask place 'enter self)
+  )
+
+  ; Methods
   (method (type) 'person)
+
   (method (look-around)
-    (map (lambda (obj) (ask obj 'name))
-	 (filter (lambda (thing) (not (eq? thing self)))
-		 (append (ask place 'things) (ask place 'people)))))
+    (map 
+      (lambda 
+        (obj) 
+        (ask obj 'name)
+      )
+	    (filter 
+        (lambda 
+          (thing) 
+          (not (eq? thing self))
+        )
+		    (append 
+          (ask place 'things) 
+          (ask place 'people)
+        )
+      )
+    )
+  )
+
   (method (take thing)
-    (cond ((not (thing? thing)) (error "Not a thing" thing))
-	  ((not (memq thing (ask place 'things)))
-	   (error "Thing taken not at this place"
-		  (list (ask place 'name) thing)))
-	  ((memq thing possessions) (error "You already have it!"))
-	  (else
-	   (announce-take name thing)
-	   (set! possessions (cons thing possessions))
-	       
-	   ;; If somebody already has this object...
-	   (for-each
-	    (lambda (pers)
-	      (if (and (not (eq? pers self)) ; ignore myself
-		       (memq thing (ask pers 'possessions)))
-		  (begin
-		   (ask pers 'lose thing)
-		   (have-fit pers))))
-	    (ask place 'people))
-	       
-	   (ask thing 'change-possessor self)
-	   'taken)))
+    (cond 
+      ((not (thing? thing)) (error "Not a thing" thing))
+	    ((not (memq thing (ask place 'things)))
+	      (error 
+          "Thing taken not at this place"
+		      (list (ask place 'name) thing)
+        )
+      )
+	    ((memq thing possessions) (error "You already have it!"))
+	    (else
+	      (announce-take name thing)
+	      (set! possessions (cons thing possessions))
+	      ;; If somebody already has this object...
+	      (for-each
+	        (lambda 
+            (pers)
+	          (if (and (not (eq? pers self)) ; ignore myself
+		          (memq thing (ask pers 'possessions)))
+		          (begin
+		            (ask pers 'lose thing)
+		            (have-fit pers)
+              )
+            )
+          )
+	        (ask place 'people)
+        )
+	      (ask thing 'change-possessor self)
+	      'taken
+      )
+    )
+  )
 
   (method (lose thing)
     (set! possessions (delete thing possessions))
     (ask thing 'change-possessor 'no-one)
-    'lost)
+    'lost
+  )
+
   (method (talk) (print saying))
   (method (set-talk string) (set! saying string))
   (method (exits) (ask place 'exits))
   (method (notice person) (ask self 'talk))
+
   (method (go direction)
-    (let ((new-place (ask place 'look-in direction)))
-      (cond ((null? new-place)
-	     (error "Can't go" direction))
-	    (else
-	     (ask place 'exit self)
-	     (announce-move name place new-place)
-	     (for-each
-	      (lambda (p)
-		(ask place 'gone p)
-		(ask new-place 'appear p))
-	      possessions)
-	     (set! place new-place)
-	     (ask new-place 'enter self))))) )
+    (let 
+      ((new-place (ask place 'look-in direction)))
+      (cond 
+        ((null? new-place)
+	        (error "Can't go" direction)
+        )
+	      (else
+	        (ask place 'exit self)
+	        (announce-move name place new-place)
+	        (for-each
+	          (lambda 
+              (p)
+		          (ask place 'gone p)
+		          (ask new-place 'appear p)
+            )
+	          possessions
+          )
+	        (set! place new-place)
+	        (ask new-place 'enter self)
+        )
+      )
+    )
+  ) 
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; THING
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define thing
-  (let ()
+  (let 
+    ()
     (lambda (class-message)
       (cond
-       ((eq? class-message 'instantiate)
-	(lambda (name)
-	  (let ((self '()) (possessor 'no-one))
-	    (define (dispatch message)
-	      (cond
-	       ((eq? message 'initialize)
-		(lambda (value-for-self)
-		  (set! self value-for-self)))
-	       ((eq? message 'send-usual-to-parent)
-		(error "Can't use USUAL without a parent." 'thing))
-	       ((eq? message 'name) (lambda () name))
-	       ((eq? message 'possessor) (lambda () possessor))
-	       ((eq? message 'type) (lambda () 'thing))
-	       ((eq? message 'change-possessor)
-		(lambda (new-possessor)
-		  (set! possessor new-possessor)))
-	       (else (no-method 'thing))))
-	    dispatch)))
-       (else (error "Bad message to class" class-message))))))
+        ((eq? class-message 'instantiate)
+	        (lambda 
+            (name)
+	          (let 
+              ((self '()) (possessor 'no-one))
+	            (define (dispatch message)
+	              (cond
+	                ((eq? message 'initialize)
+	        	        (lambda 
+                      (value-for-self)
+	        	          (set! self value-for-self)
+                    )
+                  )
+	                ((eq? message 'send-usual-to-parent)
+	        	        (error "Can't use USUAL without a parent." 'thing)
+                  )
+	                ((eq? message 'name) (lambda () name))
+	                ((eq? message 'possessor) (lambda () possessor))
+	                ((eq? message 'type) (lambda () 'thing))
+	                ((eq? message 'change-possessor)
+	        	        (lambda 
+                      (new-possessor)
+	        	          (set! possessor new-possessor)
+                    )
+                  )
+	                (else (no-method 'thing))
+                )
+              )
+	            dispatch
+            )
+          )
+        )
+        (else 
+          (error "Bad message to class" class-message)
+        )
+      )
+    )
+  )
+)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Implementation of thieves for part two
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define *foods* '(pizza potstickers coffee))
 
 (define (edible? thing)
-  (member? (ask thing 'name) *foods*))
+  (member? (ask thing 'name) *foods*)
+)
 
 (define-class (thief initial-name initial-place)
+  ; Child of person class
   (parent (person initial-name initial-place))
+
+  ; Instance variables
   (instance-vars
-   (behavior 'steal))
+   (behavior 'steal)
+  )
+
+  ; Methods
   (method (type) 'thief)
 
   (method (notice person)
     (if (eq? behavior 'run)
-	(ask self 'go (pick-random (ask (usual 'place) 'exits)))
-	(let ((food-things
-	       (filter (lambda (thing)
-			 (and (edible? thing)
-			      (not (eq? (ask thing 'possessor) self))))
-		       (ask (usual 'place) 'things))))
-	  (if (not (null? food-things))
-	      (begin
-	       (ask self 'take (car food-things))
-	       (set! behavior 'run)
-	       (ask self 'notice person)) )))) )
+	    (ask self 'go (pick-random (ask (usual 'place) 'exits)))
+	    (let 
+        ((food-things
+	        (filter 
+            (lambda (thing)
+	    		    (and 
+                (edible? thing)
+	    		      (not (eq? (ask thing 'possessor) self))
+              )
+            )
+	    	    (ask (usual 'place) 'things)
+          )
+        ))
+	      (if (not (null? food-things))
+	        (begin
+	          (ask self 'take (car food-things))
+	          (set! behavior 'run)
+	          (ask self 'notice person)
+          ) 
+        )
+      )
+    )
+  )
+)
 
 (define-class (police initial-name initial-place)
 
@@ -184,17 +326,24 @@
   (newline)
   (print (ask who 'exits))
   (display "?  > ")
-  (let ((dir (read)))
+  (let 
+    ((dir (read)))
     (if (equal? dir 'stop)
-	(newline)
-	(begin (print (ask who 'go dir))
-	       (move-loop who)))))
+	    (newline)
+	    (begin 
+        (print (ask who 'go dir))
+	      (move-loop who)
+      )
+    )
+  )
+)
 
 
 ;; One-way paths connect individual places.
 
 (define (can-go from direction to)
-  (ask from 'new-neighbor direction to))
+  (ask from 'new-neighbor direction to)
+)
 
 
 (define (announce-take name thing)
@@ -202,7 +351,8 @@
   (display name)
   (display " took ")
   (display (ask thing 'name))
-  (newline))
+  (newline)
+)
 
 (define (announce-move name old-place new-place)
   (newline)
@@ -212,28 +362,46 @@
   (display (ask old-place 'name))
   (display " to ")
   (display (ask new-place 'name))
-  (newline))
+  (newline)
+)
 
 (define (have-fit p)
   (newline)
   (display "Yaaah! ")
   (display (ask p 'name))
   (display " is upset!")
-  (newline))
+  (newline)
+)
 
 
 (define (pick-random set)
-  (nth (random (length set)) set))
+  (nth (random (length set)) set)
+)
 
 (define (delete thing stuff)
   (cond ((null? stuff) '())
-	((eq? thing (car stuff)) (cdr stuff))
-	(else (cons (car stuff) (delete thing (cdr stuff)))) ))
+	  ((eq? thing (car stuff)) 
+     (cdr stuff)
+    )
+	  (else 
+      (cons 
+        (car stuff) 
+        (delete thing (cdr stuff))
+      )
+    )
+  )
+)
 
 (define (person? obj)
-  (and (procedure? obj)
-       (member? (ask obj 'type) '(person police thief))))
+  (and 
+    (procedure? obj)
+    (member? (ask obj 'type) '(person police thief))
+  )
+)
 
 (define (thing? obj)
-  (and (procedure? obj)
-       (eq? (ask obj 'type) 'thing)))
+  (and 
+    (procedure? obj)
+    (eq? (ask obj 'type) 'thing)
+  )
+)
