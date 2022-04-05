33f55e4040e9661e009a0b8147af3e55c57d702e
Alba Martínez Rodríguez <bertaalba1@gmail.com>
2022-04-05 20:51:15 +0200

    [Book] Continue T10

--- BProblems/T3/P05_03.scm
+++ BProblems/T3/P05_03.scm
@@ -98,31 +98,31 @@
 
 ; S_n+1 - (S_n+1 - S_n)^2/(S_n-1 - 2Sn + S_n+1)
 
-(define (euler-transform s)
-  (let 
-    (
-      (s0 (stream-ref s 0)) ; Sn−1
-      (s1 (stream-ref s 1)) ; Sn
-      (s2 (stream-ref s 2)) ; Sn+1
-    ) 
-    (cons-stream 
-      (- 
-        ; S_n+1
-        s2 
-        (/ 
-          ; (S_n+1 - S_n)^2
-          (square (- s2 s1))
-          ; (S_n-1 - 2Sn + S_n+1)
-          (+ s0 (* -2 s1) s2)
-        )
-      )
-      ; Keep applying
-      (euler-transform (stream-cdr s))
-    )
-  )
-)
-
-(display-stream (euler-transform pi-stream))
+; (define (euler-transform s)
+;   (let 
+;     (
+;       (s0 (stream-ref s 0)) ; Sn−1
+;       (s1 (stream-ref s 1)) ; Sn
+;       (s2 (stream-ref s 2)) ; Sn+1
+;     ) 
+;     (cons-stream 
+;       (- 
+;         ; S_n+1
+;         s2 
+;         (/ 
+;           ; (S_n+1 - S_n)^2
+;           (square (- s2 s1))
+;           ; (S_n-1 - 2Sn + S_n+1)
+;           (+ s0 (* -2 s1) s2)
+;         )
+;       )
+;       ; Keep applying
+;       (euler-transform (stream-cdr s))
+;     )
+;   )
+; )
+; 
+; (display-stream (euler-transform pi-stream))
 ; 3.166666666666667
 ; 3.1333333333333337
 ; 3.1452380952380956
@@ -164,4 +164,167 @@
 
 ; Accelerator: euler-transforma
 ; Series: pi-stream
-(display-stream (accelerated-sequence euler-transform pi-stream))
+;; MISSING PI-STREAM
+; (display-stream (accelerated-sequence euler-transform pi-stream))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; INFINITE STREAMS OF PAIRS
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; MISSING INT-PAIRS
+
+; Obtain all the integers i,j where i + j is a prime number
+; (stream-filter
+;   (lambda 
+;     (pair) 
+;     (prime? 
+;       (+ (car pair) (cadr pair))
+;     )
+;   )
+;   int-pairs
+; )
+
+; Where int-pairs all all the pairs of the form (i, j) where i and j are integers and i <= j
+; The problem is generating the pairs of integers, because the streams are infinite
+; So imagine we have two streams: S and T, such that the pairs are
+; (S0, T0), (S0, T1), (S0, T2), ...
+; (S1, T0), (S1, T1), (S1, T2), ...
+; (S2, T0), (S2, T1), (S2, T2), ...
+; ...
+
+; To fetch unique pairs, we will only be concerned with the upper triangle:
+; (S0, T0), (S0, T1), (S0, T2), ...
+;           (S1, T1), (S1, T2), ...
+;                     (S2, T2), ...
+; ...
+
+; We now divide the pairs into three sections:
+
+; (S0, T0),|(S0, T1), (S0, T2), ...
+; ---------------------------------
+;          |(S1, T1), (S1, T2), ...
+;          |          (S2, T2), ...
+; ...      |
+
+; One is (S0, T0)
+; Another are the elements in the same row as (S0, T0), this not included
+; And finally, the last section are the remaining elements
+
+; This last section is formed, recursively, for each Sn with (list (stream-cdr S) (stream-cdr T))
+; The second section is formed as follows:
+
+;  >>> (stream-map 
+;  >>>   (lambda 
+;  >>>     (x) 
+;  >>>     ; Make a pair with S0 (not necessarily) and x(=Tn), so we create:
+;  >>>     ; 1. (S0, T1)
+;  >>>     ; 2. (S0, T2)
+;  >>>     ; 2. (S0, T3), etc
+;  >>>     (list (stream-car s) x)
+;  >>>   )
+;  >>>   ; For each Tn in the stream T, excluding T0 with cdr
+;  >>>   (stream-cdr t)
+;  >>> )
+
+; So we can form the stream of pairs like:
+
+; - s and t are streams
+(define (pairs s t)
+  (cons-stream
+    ; First section
+    (list (stream-car s) (stream-car t))
+    ; Second and third section
+    (interleave
+      ; Second section: explained above
+      (stream-map 
+        (lambda (x) (list (stream-car s) x))
+        (stream-cdr t)
+      )
+      ; Third section: apply this procedure recursively*
+      (pairs (stream-cdr s) (stream-cdr t))
+    )
+  )
+)
+
+; The recursion of the third section:
+
+; For streams S and T, where {a1}, {a2}, {a3} are the different sections we defined
+; > Starting point
+; > With the model we define earlier
+; > (S0, T0),|(S0, T1), (S0, T2), ...
+; > ---------------------------------
+; >          |(S1, T1), (S1, T2), ...
+; >          |          (S2, T2), ...
+; > ...      |
+; > {a1} = (S0, T0) 
+; > {a2} = (S0, T1), (S0, T2), ...
+; > {a3} = (pairs (cdr S) (cdr T)) = (pairs (S1 S2 S3 ...) (T1 T2 T3 ...))
+; >>> We enter the recursive call 
+; >>> With the model we define earlier, we remove the first row, and move the divider 
+; >>> one row under:
+; >>>          (S1, T1), |(S1, T2), ...
+; >>> ---------------------------------
+; >>>                    |(S2, T2), ...
+; >>> ...      
+; >>> {a1} = (S1, T1) 
+; >>> {a2} = (S1, T2), (S1, T3), ...
+; >>> {a3} = (pairs (cdr S) (cdr T)) = (pairs (S2 S3 S4 ...) (T2 T3 T4 ...))
+
+; How to combine the two last sections of the pairs:
+
+; Because streams are infinite we want to take elements from the two streams alternatively
+; instead of generating all the pairs from one stream. In our case this would mean: we do 
+; not want to only feed our filter pairs from the second section, and not be able to 
+; make use of the third section. But with interleave we alternate one pair of the second section
+; with one pair of the third section.
+
+(define (interleave s1 s2)
+  ; If the stream is emtpy, there is nothing
+  ; to alternate with, so return s2
+  (if (stream-null? s1)
+    s2
+    ; Else make a stream made up of:
+    ; 1. Element of s1
+    ; 2. Rest of elements of s1 and s2, but not
+    ;    s2 acts as s1, so the next call (stream-car)
+    ;    will be applied over s2, and will be the next element after 
+    ;    the element in step 1
+    (cons-stream 
+      (stream-car s1)
+      (interleave s2 (stream-cdr s1))
+    )
+  )
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; STREAMS AS SIGNALS: INTEGRATOR
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; For an input stream x=(x_i) an initial value C and an increment dt:
+
+; S_i = C + sum(x_j*dt), from j=1 to i
+
+; this returns the stream of values S=(S_i)
+
+; - integrand = input stream x
+; - initial-value = C
+; - dt = dt
+(define (integral integrand initial-value dt)
+  ; Create the stream S
+  (define int
+    ; Make stream, where the first value is C
+    (cons-stream 
+      initial-value
+      ; Start summation between two streams A and B, where:
+      ; - A(=integrand): input stream x
+      ; - B: itself up until current element
+      (add-streams 
+        ; multiply every element of the input stream x by dt
+        (scale-stream integrand dt)
+        int
+      )
+    )
+  )
+  int
+)
