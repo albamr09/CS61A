6055a5543876d11ca6879f2a8b5c816feb606561
Alba Martínez Rodríguez <bertaalba1@gmail.com>
2022-04-15 21:00:43 +0200

    [Book] Start T11

---
+++ BProblems/T4/P01_01.scm
@@ -0,0 +1,45 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; EVAL DEFINITION
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define (eval exp env)
+  (cond 
+    ; If it is a primitive self-evaluating expression (number), return itself
+    ((self-evaluating? exp) exp)
+    ; If it is a variable, search its value
+    ((variable? exp) (lookup-variable-value exp env))
+    ; If it is a quoted expression, return the expression that was quoted
+    ((quoted? exp) (text-of-quotation exp))
+    ; Call eval recursively to compute the value to be associated with the variable
+    ((assignment? exp) (eval-assignment exp env))
+    ((definition? exp) (eval-definition exp env))
+    ; Check if the predicate is true to decice which argument to evaluate (first or second)
+    ((if? exp) (eval-if exp env))
+    ; If it is a lambda expression pack the parameters and the body along with the environment 
+    ((lambda? exp) 
+      (make-procedure 
+        (lambda-parameters exp)
+        (lambda-body exp)
+        env
+      )
+    )
+    ; If it is a begin expression, evaluate the sequence of expressions in order
+    ((begin? exp)
+      (eval-sequence (begin-actions exp) env)
+    )
+    ; If it is a cond expression convert it to a set of if expressions
+    ((cond? exp) (eval (cond->if exp) env))
+    ; If it is the application of a procedure
+    ((application? exp)
+      ; Apply the operator obtained to the list of operands
+      (apply 
+        ; Evaluate recursively the operator
+        (eval (operator exp) env)
+        ; Evaluate recursively the operands
+        (list-of-values (operands exp) env)
+      )
+    )
+    (else
+    (error "Unknown expression type: EVAL" exp))
+  )
+)
