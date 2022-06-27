cb00d6e308068de3ee04969854aafe0734094996
Alba Martínez Rodríguez <bertaalba1@gmail.com>
2022-06-27 19:06:01 +0200

    [L12] Start E04

---
+++ Labs/T4/L12/E03_03.scm
@@ -0,0 +1,4 @@
+; eval uses actual-value rather than eval to evaluate the operator before passing it to apply, 
+; in order to force the value of the operator. Give an example that demonstrates the need for this forcing.
+
+; Because the operator might be in itself an expression to be evaluated and has been delayed 
